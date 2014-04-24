bd914f03c97332a0adb0f6df4af93e72eb33df66
BURCU <burcukolbay@gmail.com>
2014-04-24 11:53:52 +0300

    Create cleaningupfullname.php

---
+++ cleaningupfullname.php
@@ -0,0 +1,28 @@
+<?php
+
+
+echo fix_names("WILLIAM", "henry", "gatES");
+function fix_names($n1, $n2, $n3)
+{
+$n1 = ucfirst(strtolower($n1)); //ucfirst ilk harfi büyük yaptı
+$n2 = ucfirst(strtolower($n2));
+$n3 = ucfirst(strtolower($n3));
+return $n1 . " " . $n2 . " " . $n3;
+}
+/*<?php
+$a1 = "WILLIAM";
+$a2 = "henry";
+$a3 = "gatES";
+echo $a1 . " " . $a2 . " " . $a3 . "<br />";
+fix_names($a1, $a2, $a3);
+echo $a1 . " " . $a2 . " " . $a3;
+function fix_names(&$n1, &$n2, &$n3)
+{
+$n1 = ucfirst(strtolower($n1));
+$n2 = ucfirst(strtolower($n2));
+$n3 = ucfirst(strtolower($n3));
+}
+?>
+ */
+
+?>
